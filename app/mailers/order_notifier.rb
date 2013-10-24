0d8114b1e1bc79f8eb174b078ff25b3f24bb14fb
Yuin <yhuang1223@gmail.com>
2013-10-24 15:36:29 +0100

    added Order model, Mailer order_notifier, created confirmation_email

---
+++ app/mailers/order_notifier.rb
@@ -0,0 +1,11 @@
+class OrderNotifier < ActionMailer::Base
+
+	default from: "no-reply@mcandyshop.com"
+
+	def confirmation_email(number_of_items)
+	   @number_of_items = number_of_items
+
+	   mail(to: 'someuser@example.com', subject: 'Order Confirmation')
+	end 
+
+end
--- spec/features/order_emails_feature_spec.rb
+++ spec/features/order_emails_feature_spec.rb
@@ -1,8 +1,13 @@
 require 'spec_helper'
 
+def emails
+	ActionMailer::Base.deliveries
+end
+
+
 describe 'order confirmation' do
 
-	xit 'should be send after order' do
+	it 'should be send after order' do
 		# e.g. internal rails methods, deliver method is called. -> complicated.
 		expect { Order.create(number_of_items: 3) }.to change { emails.count }.by 1
 	end
@@ -13,13 +18,13 @@ describe 'order confirmation' do
 	# expect(emails.count).to eq (old_email_count + 1 }
 	# end
 
-	xit 'should have the subject "Order confirmation"' do
+	it 'should have the subject "Order confirmation"' do
 		Order.create(number_of_items: 3)
 		expect(emails.last.subject).to eq "Order Confirmation"
 	end
 
 
-	xit 'should name the number of items' do
+	it 'should name the number of items' do
 		Order.create(number_of_items: 3)
 		expect(emails.last.body).to match "3 items"
 	end
