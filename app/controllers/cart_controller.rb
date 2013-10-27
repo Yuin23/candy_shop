9ad77027017f623b4542692c516ce59d3a161c9e
mvandeloo <martin.vandeloo@googlemail.com>
2013-10-27 22:21:30 +0000

    sum in cart added, index page with products

--- app/controllers/cart_controller.rb
+++
@@ -1,7 +0,0 @@
-class CartsController < ApplicationController
-def update
-	product = Product.find params[:cart][:id][:product_id]
-	current_cart.products << product 
-	redirect_to product_path(product)
-end
-end
